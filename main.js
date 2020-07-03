3ce243a887dcd5fb4333bfdbbffe96b8b4bad8a9
EM <54121259+emathieson@users.noreply.github.com>
2020-07-03 17:53:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const api = {
     key: "39e69962ae6d03fcd32496a060164137",
-    baseurl: "https://api.openweathermap.org/data/2.5"
+    baseurl: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
@@ -14,8 +14,8 @@ function setQuery(evt) {
 }
 
 function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-    .then(weather => {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  .then(weather => {
         return weather.json();
     }).then(displayResults);
 }
